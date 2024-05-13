84547f960f638eee32d21b3ae48397c9add7782a
Oriol Cayon <ocayon@tudelft.nl>
2024-05-13 15:45:03 +0200

    EKFOutput class and output functions in a separate file

---
+++ src/awes_ekf/ekf/ekf_output.py
@@ -0,0 +1,130 @@
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+from config import z0, kappa
+from utils import calculate_euler_from_reference_frame, calculate_airflow_angles, calculate_reference_frame_euler
+ 
+
+@dataclass
+class EKFOutput:
+    kite_pos_x: float
+    kite_pos_y: float
+    kite_pos_z: float
+    kite_vel_x: float
+    kite_vel_y: float
+    kite_vel_z: float
+    wind_velocity: float
+    wind_direction: float
+    roll: float
+    pitch: float
+    yaw: float
+    tether_length: float
+    kite_aoa: float
+    kite_sideslip: float
+    CL: float
+    CD: float
+    CS: float
+    elevation_first_element: float
+    azimuth_first_element: float
+    cd_tether: float = None
+    cd_kcu: float = None
+    z_wind: float = None
+    k_steering_law: float = None
+    kcu_roll: float = None
+    kcu_pitch: float = None
+    kcu_yaw: float = None
+    
+def create_ekf_output(x, u, kite, tether,kcu, model_specs):
+    """Store results in a list of instances of the class EKFOutput"""
+    # Store tether force and tether model results
+    kite_pos = x[0:3]
+    kite_vel = x[3:6]
+    if model_specs.log_profile:
+        wind_vel  = x[6]/kappa*np.log(x[2]/z0)
+        wind_dir = x[7]
+        z_wind = x[8]
+        vw = np.array([wind_vel*np.cos(wind_dir), wind_vel*np.sin(wind_dir), z_wind])
+    else:
+        vw = x[6:9]
+        wind_vel = np.linalg.norm(vw)
+        wind_dir = np.arctan2(vw[1],vw[0])
+        z_wind = vw[2]
+    tension_ground = u[1]
+    tether_length = x[12]
+    elevation_0 = x[13]
+    azimuth_0 = x[14]
+
+    if kcu.data_available:
+        kcu_acc = u[2:5]
+        kcu_vel = u[5:8]
+        kite_acc = None
+    else:
+        kcu_acc = None
+        kcu_vel = None
+        kite_acc = u[2:5]
+
+    args = (model_specs.n_tether_elements, kite_pos, kite_vel, vw, kite, kcu, tension_ground )
+    opt_guess = [elevation_0, azimuth_0, tether_length]
+    res = tether.calculate_tether_shape(opt_guess, *args, a_kite = kite_acc, a_kcu = kcu_acc, v_kcu = kcu_vel, return_values=True)
+    dcm_b2w = res[2]
+    dcm_t2w = res[3]
+    euler_angles = calculate_euler_from_reference_frame(dcm_b2w)
+    euler_angles1 = calculate_euler_from_reference_frame(dcm_t2w)
+    cd_kcu = res[-5]
+    cd_tether = res[-4]
+    if model_specs.model_yaw:
+        ex, ey, ez = calculate_reference_frame_euler( euler_angles[0], 
+                                                     euler_angles[1], 
+                                                     x[15]*180/np.pi, 
+                                                     bodyFrame='NED')
+        dcm = np.vstack(([ex], [ey], [ez])).T
+        airflow_angles = calculate_airflow_angles(dcm, kite_vel, vw)
+    else:
+        airflow_angles = calculate_airflow_angles(dcm_b2w, kite_vel, vw)
+
+    # Unpack position and velocity vectors
+    kite_pos_x, kite_pos_y, kite_pos_z = x[0:3]
+    kite_vel_x, kite_vel_y, kite_vel_z = x[3:6]
+
+    # Create an instance of EKFOutput with unpacked vectors
+    ekf_output = EKFOutput(
+        kite_pos_x = kite_pos_x,
+        kite_pos_y = kite_pos_y,
+        kite_pos_z = kite_pos_z,
+        kite_vel_x = kite_vel_x,
+        kite_vel_y = kite_vel_y,
+        kite_vel_z = kite_vel_z,
+        wind_velocity = wind_vel,
+        wind_direction = wind_dir,
+        roll = euler_angles[0],
+        pitch = euler_angles[1],
+        yaw = euler_angles[2],
+        tether_length = x[12],
+        kite_aoa = airflow_angles[0],
+        kite_sideslip = airflow_angles[1],
+        CL = x[9],
+        CD = x[10],
+        CS = x[11],
+        elevation_first_element = x[13],
+        azimuth_first_element = x[14],
+        cd_kcu = cd_kcu,
+        cd_tether = cd_tether,
+        z_wind = z_wind,
+        kcu_roll = euler_angles1[0]
+    )
+    
+    if model_specs.model_yaw:
+        ekf_output.k_steering_law = x[16]
+        ekf_output.yaw = x[15]
+                            
+    return ekf_output
+
+def convert_ekf_output_to_df(ekf_output_list):
+    """Convert list of EKFOutput instances to DataFrame by using the __dict__ of each instance."""
+    # List comprehension that converts each instance to a dictionary
+    data_dicts = [vars(output) for output in ekf_output_list]
+    
+    # Create DataFrame directly from list of dictionaries
+    ekf_output_df = pd.DataFrame(data_dicts)
+    
+    return ekf_output_df
