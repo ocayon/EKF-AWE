3bace4fa9be2733c0e18382ed4e446ac4c9490cb
ocayon <94445351+ocayon@users.noreply.github.com>
2024-05-30 18:41:27 +0200

    Add thrust input

--- src/awes_ekf/ekf/ekf_input.py
+++ src/awes_ekf/ekf/ekf_input.py
@@ -3,18 +3,19 @@ import numpy as np
 
 @dataclass
 class EKFInput:
-    kite_pos: np.array      # Kite position in ENU coordinates
-    kite_vel: np.array      # Kite velocity in ENU coordinates
-    kite_acc: np.array      # Kite acceleration in ENU coordinates
+    kite_pos: np.ndarray      # Kite position in ENU coordinates
+    kite_vel: np.ndarray      # Kite velocity in ENU coordinates
+    kite_acc: np.ndarray      # Kite acceleration in ENU coordinates
     ts: float              # Timestep (s)
     tether_force: float     # Ground tether force
     apparent_windspeed: float = None # Apparent windspeed
     tether_length: float = None     # Tether length
-    kite_aoa: np.array = None      # Kite angle of attack
-    kcu_vel: np.array = None    # KCU velocity in ENU coordinates
-    kcu_acc: np.array = None    # KCU acceleration in ENU coordinates
+    kite_aoa: np.ndarray = None      # Kite angle of attack
+    kcu_vel: np.ndarray = None    # KCU velocity in ENU coordinates
+    kcu_acc: np.ndarray = None    # KCU acceleration in ENU coordinates
     reelout_speed: float = None  # Reelout speed
     elevation: float = None      # Elevation angle
     azimuth: float = None        # Azimuth angle
     kite_yaw: float = None            # Yaw angle
     steering_input: float = None # Steering input
+    thrust_force: np.ndarray = None # Thrust force
