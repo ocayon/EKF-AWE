3a7da9225756e217c9412cc412232fc6e1f3234d
ocayon <94445351+ocayon@users.noreply.github.com>
2024-06-27 15:46:06 +0200

    Remove functions that are not used in dynamic model

--- src/awes_ekf/ekf/dynamic_model.py
+++ src/awes_ekf/ekf/dynamic_model.py
@@ -15,24 +15,9 @@ class DynamicModel:
         self.fx = kite.get_fx(tether)
         self.calc_fx = ca.Function("calc_fx", [self.x, self.u, self.x0], [self.fx])
 
-    def get_fx_fun(self, kite, tether, kcu):
-        return ca.Function(
-            "calc_Fx", [self.x, self.u, self.x0], [self.get_fx(kite, tether, kcu)]
-        )
-
     def get_fx_jac(self):
 
         return ca.simplify(ca.jacobian(self.fx, self.x))
 
     def get_fx_jac_fun(self):
         return ca.Function("calc_Fx", [self.x, self.u, self.x0], [self.get_fx_jac()])
-
-    def propagate(self, x, u, ts):
-
-        fx = self.calc_fx(self.x, self.u, x)
-
-        # Define ODE system
-        dae = {"x": self.x, "p": self.u, "ode": fx}  # Define ODE system
-        integrator = ca.integrator("intg", "cvodes", dae, 0, ts)  # Define integrator
-
-        return np.array(integrator(x0=x, p=u)["xf"].T)
